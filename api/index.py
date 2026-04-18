f4a4a5b120a8dfb6864142ea2d6d68a0ad45c2f0
Mohad <mohadzaheer77@gmail.com>
2026-04-18 09:53:02 +0500

    1.2.1

--- api/index.py
+++ api/index.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, render_template, jsonify
 import pickle
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, '..', 'models', 'model.pkl')
+vectorizer_path = os.path.join(BASE_DIR, '..', 'models', 'tfidfvectorizer.pkl')
 
 app = Flask(__name__, 
             template_folder='../templates', 
             static_folder='../static')
 
 try:
-    model = pickle.load(open('models/model.pkl', 'rb'))
-    vectorizer = pickle.load(open('models/tfidfvectorizer.pkl', 'rb'))
+    model = pickle.load(open(model_path, 'rb'))
+    vectorizer = pickle.load(open(vectorizer_path, 'rb'))
 except FileNotFoundError:
     print("Error: Model or Vectorizer files not found.")
 
